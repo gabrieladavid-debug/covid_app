727bf16ba844344e8acc7bfa6f14fd2d0e193fe8
gabrieladavid-debug <gabriela.david@upr.edu>
2025-11-28 12:31:14 +0000

    tcjj

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
